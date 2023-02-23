debddcda513a2f593a6d75172381fda9ef7bf5ea
zeusmoney <ezechukwuchibuikemoses@gmail.com>
2023-02-23 15:13:58 +0100

    modify files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 /* eslint-disable quotes */
 import "./style.css";
 import display from "./todo.js";
+import { clearCompleted } from "./mark.js";
 
-const editTodoForm = document.querySelector("#todo-form");
 const enterBtn = document.querySelector(".enter-btn");
-const editTodoFormInput = document.querySelector(".todo-edit input");
+const clearCheck = document.querySelector(".clear");
+
+clearCheck.addEventListener("click", () => {
+  const task = display.getTodo();
+  const todo = clearCompleted(task);
+  localStorage.setItem("todo", JSON.stringify(todo));
+  display.loadTodo(todo);
+});
 
 window.addEventListener("DOMContentLoaded", () => {
   const task = display.getTodo();
@@ -13,13 +20,4 @@ window.addEventListener("DOMContentLoaded", () => {
 
 enterBtn.addEventListener("click", () => {
   display.addTodo();
-});
-
-editTodoForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const id = Number(editTodoFormInput.getAttribute("id"));
-  display.updateTaskInput(editTodoFormInput.value, id);
-  editTodoFormInput.value = "";
-  document.querySelector(".type-task").style.display = "block";
-  editTodoForm.style.display = "none";
 });
